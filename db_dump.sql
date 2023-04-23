adb47125518416df1fddb17f7e0640f64ff88ee4
s4mP4t03 <101205576+s4mP4t03@users.noreply.github.com>
2023-04-23 16:36:10 +0200

    Add files via upload

--- db_dump.sql
+++ db_dump.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: db_name
 -- ------------------------------------------------------
--- Server version	8.0.32-0ubuntu0.22.04.2
+-- Server version	8.0.32-0ubuntu0.20.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,34 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Affitti`
+--
+
+DROP TABLE IF EXISTS `Affitti`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Affitti` (
+  `IdAffitto` int NOT NULL AUTO_INCREMENT,
+  `data` date DEFAULT NULL,
+  `Importo` int DEFAULT NULL,
+  `codappartamento` int DEFAULT NULL,
+  PRIMARY KEY (`IdAffitto`),
+  KEY `codappartamento` (`codappartamento`),
+  CONSTRAINT `Affitti_ibfk_1` FOREIGN KEY (`codappartamento`) REFERENCES `Appartamenti` (`CodAppartamenti`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Affitti`
+--
+
+LOCK TABLES `Affitti` WRITE;
+/*!40000 ALTER TABLE `Affitti` DISABLE KEYS */;
+INSERT INTO `Affitti` VALUES (1,'2015-03-25',500,6),(2,'2019-03-25',333,4),(3,'2020-03-25',164,4),(4,'2010-03-25',164,2);
+/*!40000 ALTER TABLE `Affitti` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Appartamenti`
@@ -32,7 +60,7 @@ CREATE TABLE `Appartamenti` (
   PRIMARY KEY (`CodAppartamenti`),
   KEY `codproprietario` (`codproprietario`),
   CONSTRAINT `Appartamenti_ibfk_1` FOREIGN KEY (`codproprietario`) REFERENCES `Proprietario` (`CodProprietario`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +69,7 @@ CREATE TABLE `Appartamenti` (
 
 LOCK TABLES `Appartamenti` WRITE;
 /*!40000 ALTER TABLE `Appartamenti` DISABLE KEYS */;
-INSERT INTO `Appartamenti` VALUES (1,95,2,'Lecco','Ghezzi',1),(2,105,2,'Lecco','Ghezzi',1),(4,105,2,'Como','Ghezzi',4),(5,300,2,'Lecco','Ghezzi',2);
+INSERT INTO `Appartamenti` VALUES (1,95,2,'Lecco','Ghezzi',1),(2,105,2,'Lecco','Ghezzi',1),(4,105,2,'Como','Ghezzi',4),(5,300,2,'Lecco','Ghezzi',2),(6,NULL,NULL,'Milano','Giulia è gay',5);
 /*!40000 ALTER TABLE `Appartamenti` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,4 +140,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-20 11:21:29
+-- Dump completed on 2023-04-23 16:10:50
